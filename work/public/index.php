6d1ea1cc1eff1065bf23941fc37c2a03d1daeb22
oshinoshin <donateluro@gmail.com>
2021-05-21 16:01:50 +0900

    Todoクラスの実装

--- work/public/index.php
+++ work/public/index.php
@@ -2,35 +2,15 @@
 
 require_once(__DIR__ . '/../app/config.php');
 
-Token::create();
-
 $pdo = Database::getInstance();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  Token::validate();
-  $action = filter_input(INPUT_GET, 'action');
+$todo = new Todo($pdo);
+$todo->processPost();
+$todos = $todo->getAll();
 
-  switch ($action) {
-    case 'add':
-      addTodo($pdo);
-      break;
-    case 'toggle':
-      toggleTodo($pdo);
-      break;
-    case 'delete':
-      deleteTodo($pdo);
-      break;
-    default:
-      exit;
-  }
-
-  header('Location: ' . SITE_URL);
-  exit;
-}
-
-$todos = getTodos($pdo);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
